da23c65dc06c6efb74574390ac090cd698268d9f
MDupas <32423534+MDupas@users.noreply.github.com>
2018-01-24 02:50:44 +0100

    Debut ParserArithmetique : enums Eval et Oper ; struct Ast.

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    println!("Hello, world!");
+    //1 - Lire un fichier
+    // Boucle
+        //2 - Lire un caractère
+        //3 - Déterminer quoi faire du caractère (augmenter chiffre, opération)
+        //4 - Construire AST (on s'en fout des prios)
+
+    let instruction = "3 + 1 - 21";
+
+}
+
+enum Evaluable {
+    Numb(int),
+    Oper(Ast)
+}
+
+enum Operator {
+    Add,
+    Min,
+    Mul,
+    Div
+}
+
+struct Ast {
+    a: Evaluable,
+    b: Evaluable,
+    op: Operator
+}
+
+impl Ast {
+    fn set_a(&self, new_a: Evaluable) -> &Ast {
+        a = newA;
+        &self
+    }
+
+    fn set_b(&self, new_b: Evaluable) -> &Ast {
+        b = newB;
+        &self
+    }
+
+    fn eval(&self) -> int {
+        x = match &self.a {
+            Evaluable::Numb(num) => num,
+            Evaluable::Oper(&ast) => &ast.eval()
+        };
+        y = match &self.b {
+            Evaluable::Numb(num) => num,
+            Evaluable::Oper(&ast) => &ast.eval()
+        };
+        match &self.op {
+            Operator::Add => x + y,
+            Operator::Min => x - y,
+            Operator::Mul => x * y,
+            Operator::Div => x / y
+        }
+    }
+}
