78b2fd3f99faa3544ccd56f7eedf68e125289af3
MDupas <32423534+MDupas@users.noreply.github.com>
2018-01-28 04:17:57 +0100

    Ca a l'air de fonctionner ! for c in chars.next() -> loop { let c = ... n as i32 pointeurs un peu mieux maitrisés

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 fn main() {
     let instruction = "3 + 1 - 21";
     let mut chars = instruction.chars();
+    let mut num1 : i32 = 0;
+    let mut i = 0;
 
-    let num1 = 0;
-    for c in chars.next() { // TODO : premier tour de boucle pour capter le 1er nombre
-        let num2 = match c {
+    println!("taille : {}", chars.clone().count());
+    loop { // TODO : fonction (pour éviter duplication / apprendre des trucs)
+        let c = match chars.next() {
+            None => panic!("L'instruction de calcul est vide"),
+            Some(ch) => ch,
+        };
+        println!("index {}", i);
+        i += 1;
+        num1 = match c {
             ' ' => break,
             _ => match c.to_digit(10) {
-                None => panic!("Devrait être un bombre !"),
-                Some(n) => num1 * 10 + n,
+                None => panic!("Devrait être un nombre !"),
+                Some(n) => num1 * 10 + (n as i32),
             }
         };
+        println!("testeuh");
     }
+    println!("1er nombre : {}", num1);
     let mut eva1 = Evaluable::Numb(num1);
 
     loop {
+        println!("taille : {}", chars.clone().count());
         let c = match chars.next() {
             None => break,
             Some(new_c) => new_c,
@@ -24,37 +35,42 @@ fn main() {
             '-' => Operator::Min,
             '*' => Operator::Mul,
             '/' => Operator::Div,
-            _ => panic!("Devrait être un opérateur !")
+            ' ' => continue,
+            _ => panic!("Devrait être un opérateur !\nÉtait un \"{}\"", c)
         };
 
         chars.next(); // Normalement, juste un espace ; TODO il peut ne pas y en avoir !
 
-        let num2 = 0;
-        for c in chars.next() { // 2e boucle pour 2e nombre
-            let num2 = match c {
+        let mut num2 = 0;
+        loop { // 2e boucle pour 2e nombre
+            let c = match chars.next() {
+                None => break,
+                Some(ch) => ch,
+            };
+            num2 = match c {
                 ' ' => break,
                 _ => match c.to_digit(10) {
-                    None => panic!("Devrait être un bombre !"),
-                    Some(n) => num2 * 10 + n,
+                    None => panic!("Devrait être un nombre !\nÉtait un \"{}\"", c),
+                    Some(n) => num2 * 10 + (n as i32),
                 }
             };
         }
 
         let eva2 = Evaluable::Numb(num2);
 
-        eva1 = Evaluable::Oper(BinaryOperator::new(eva1, eva2, op));
+        eva1 = Evaluable::Oper(Box::new(BinaryOperator::new(eva1, eva2, op)));
 
     }
     let result = match eva1 {
         Evaluable::Numb(numb) => numb,
-        Evaluable::Oper(oper) => oper.eval(),
+        Evaluable::Oper(oper) => (*oper).eval(),
     };
     println!("Résultat : {}", result);
 }
 
 enum Evaluable {
-    Numb(u32),
-    Oper(BinaryOperator)
+    Numb(i32),
+    Oper(Box<BinaryOperator>)
 }
 
 enum Operator {
@@ -72,7 +88,7 @@ struct BinaryOperator {
 
 impl BinaryOperator {
 
-    fn new(a: Evaluable, b: Evaluable, op: Operator) -> BinaryOperator {
+    pub fn new(a: Evaluable, b: Evaluable, op: Operator) -> BinaryOperator {
         BinaryOperator {
             a,
             b,
@@ -80,24 +96,24 @@ impl BinaryOperator {
         }
     }
 
-    fn a(&self) -> Evaluable { // Pourrais juste mettre les fields public
-        self.a
+    pub fn a(&self) -> &Evaluable { // Pourrais juste mettre les fields public
+        &self.a
     }
 
-    fn b(&self) -> Evaluable {
-        self.b
+    pub fn b(&self) -> &Evaluable {
+        &self.b
     }
 
-    fn eval(&self) -> u32 {
-        let x = match &self.a {
-            Evaluable::Numb(num) => num,
-            Evaluable::Oper(&bin_op) => &bin_op.eval()
+    pub fn eval(&self) -> i32 {
+        let x = match &(self.a) {
+            &Evaluable::Numb(num) => num,
+            &Evaluable::Oper(ref bin_op) => (*bin_op).eval()
         };
-        let y = match &self.b {
-            Evaluable::Numb(num) => num,
-            Evaluable::Oper(&bin_op) => &bin_op.eval()
+        let y = match &(self.b) {
+            &Evaluable::Numb(num) => num,
+            &Evaluable::Oper(ref bin_op) => (*bin_op).eval()
         };
-        match &self.op {
+        match self.op {
             Operator::Add => x + y,
             Operator::Min => x - y,
             Operator::Mul => x * y,
